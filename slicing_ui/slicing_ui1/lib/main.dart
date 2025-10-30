e592e9d1cac0d064f3af9d31ac88f1c5b480c353
langley <galang2909@student.abudzar.sch.id>
2025-10-30 11:12:55 +0700

    Splach screen

---
+++ slicing_ui/slicing_ui1/lib/main.dart
@@ -0,0 +1,54 @@
+import 'dart:async'; 
+import 'package:flutter/material.dart';
+import 'package:slicing_ui1/halaman/page1.dart'; 
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const Page1()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          'assets/Group 1.png', 
+          width: 300,
+        ),
+      ),
+      
+    );
+  }
+}
